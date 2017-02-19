a1e5cfbfe149c87853e6ccd2248b764cef957fd4
Eleanor Kiefel Haggerty <eleanorakh@gmail.com>
2017-02-19 12:44:19 +1100

    Allow moving down by holding button

--- src/snake.ino
+++ src/snake.ino
@@ -13,6 +13,10 @@ void setup() {
 const int sizeOfSnake = 5;
 int snake[sizeOfSnake][2] = {{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}};
 
+// 0 = right
+// 1 = down
+int direction = 0;
+
 void loop() {
   if (!(arduboy.nextFrame())) return;
 
@@ -23,13 +27,27 @@ void loop() {
   };
 
   if (arduboy.everyXFrames(15)) {
-    int newCell[2] = {snake[sizeOfSnake - 1][0] + 1, snake[sizeOfSnake - 1][1]};
+    int newCell[2];
+
+    if (direction == 0) {
+      newCell[0] = snake[sizeOfSnake - 1][0] + 1;
+      newCell[1] = snake[sizeOfSnake - 1][1];
+    } else {
+      newCell[0] = snake[sizeOfSnake - 1][0];
+      newCell[1] = snake[sizeOfSnake - 1][1] + 1;
+    }
 
     for (int i = 0; i < sizeOfSnake; ++i) {
       memcpy(snake[i], snake[i + 1], sizeof(snake[i]));
     };
 
     memcpy(snake[sizeOfSnake - 1], newCell, sizeof(newCell));
+  }
+
+  if (arduboy.pressed(DOWN_BUTTON)) {
+    direction = 1;
+  } else {
+    direction = 0;
   }
 
   arduboy.display();
