363d5122eabe6c639237fb9928668f163b77332a
Eleanor Kiefel Haggerty <eleanorakh@gmail.com>
2017-02-19 12:29:10 +1100

    Move snake right

--- src/snake.ino
+++ src/snake.ino
@@ -10,15 +10,27 @@ void setup() {
   arduboy.setFrameRate(FRAMERATE);
 }
 
+const int sizeOfSnake = 5;
+int snake[sizeOfSnake][2] = {{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}};
+
 void loop() {
   if (!(arduboy.nextFrame())) return;
 
   arduboy.clear();
 
-  int snake[][2] = {{0, 0}, {1, 0}, {2, 0}, {2, 1}, {3, 1}, {4, 1}};
-  for (int i = 0; i < sizeof(snake); i++) {
+  for (int i = 0; i < sizeOfSnake - 1; i++) {
     arduboy.fillRect(snake[i][0] * 2, snake[i][1] * 2, 2, 2, 1);
   };
+
+  if (arduboy.everyXFrames(15)) {
+    int newCell[2] = {snake[sizeOfSnake - 1][0] + 1, snake[sizeOfSnake - 1][1]};
+
+    for (int i = 0; i < sizeOfSnake; ++i) {
+      memcpy(snake[i], snake[i + 1], sizeof(snake[i]));
+    };
+
+    memcpy(snake[sizeOfSnake - 1], newCell, sizeof(newCell));
+  }
 
   arduboy.display();
 }
